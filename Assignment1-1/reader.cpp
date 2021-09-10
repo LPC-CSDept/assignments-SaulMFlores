5479d260da896781caeb68f3ec4f3df15b3e551d
SaulMFlores <saulmflores@Live.com>
2021-09-09 23:19:07 -0700

    fixed the funky text but now its just printing 0 and name

--- Assignment1-1/reader.cpp
+++ Assignment1-1/reader.cpp
@@ -6,35 +6,45 @@ using namespace std;
 
 struct Student{
     int id;
-    char name[20];
+    string name;
     double score[2];
     double sum;
     double avg;
 };
-
-// void printrecord(){}
+        //something like
+/* void printrecord(int id_num){
+     int N = s.id.size()
+     for (int i; i<N; i++){
+         if (id_num == s.id[i]){
+             cout << s.name[i] << "\t" << s.score[1][N] << "\t" << s.score[2][N] << "\t";
+             cout << s.score[1][N] + s.score[2][N] << "\t" << (s.score[1][N] + s.score[2][N]) / 2 << "\n";
+         } 
+     }
+}*/
 
 int main(){
     ifstream ifs;
     Student s;
-    ifs.open("students.bin", ios::in|ios::binary);
+    ifs.open("students.bin", ios::out|ios::binary);
     if(!ifs){
         cout << "Failed to open. students.bin not found";
     }
-    ifs.read((char*) &s, sizeof(s));
-
-for (int i = 0; i<10; i++){
-        ifs >> s.id;
-        ifs >> s.name;
-        ifs >> s.score[0];
-        ifs >> s.score[1];
-        s.sum = s.score[0] + s.score[1];
-        s.avg = s.sum/2;
+    ifs.read((char*) &s.name, sizeof(s.name));
     cout << "Id " << s.id << "\t";
     cout << "Name " << s.name << "\t";
     cout << "Score1 " << s.score[0] << "\t";
     cout << "Score2 " << s.score[1] << "\t";
     cout << "Sum " << s.sum << "\t";
     cout << "Avg " << s.avg << "\n";
-    }
+
+
+
+ /*for (int i = 0; i<10; i++){
+        ifs >> s.id;
+        ifs >> s.name;
+        ifs >> s.score[0];
+        ifs >> s.score[1];
+        s.sum = s.score[0] + s.score[1];
+        s.avg = s.sum/2;*/
+    
 }
