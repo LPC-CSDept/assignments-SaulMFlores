1b8f017476c597a2c4ee13a94f7f64e90680fd9e
SaulMFlores <saulmflores@Live.com>
2021-09-09 22:20:31 -0700

    I think I'm on to something

--- Assignment1-1/reader.cpp
+++ Assignment1-1/reader.cpp
@@ -17,16 +17,24 @@ struct Student{
 int main(){
     ifstream ifs;
     Student s;
-    ifs.open("students.bin",ios::binary);
+    ifs.open("students.bin", ios::in|ios::binary);
     if(!ifs){
         cout << "Failed to open. students.bin not found";
     }
+    ifs.read((char*) &s, sizeof(s));
 
-
+for (int i = 0; i<10; i++){
+        ifs >> s.id;
+        ifs >> s.name;
+        ifs >> s.score[0];
+        ifs >> s.score[1];
+        s.sum = s.score[0] + s.score[1];
+        s.avg = s.sum/2;
     cout << "Id " << s.id << "\t";
     cout << "Name " << s.name << "\t";
     cout << "Score1 " << s.score[0] << "\t";
     cout << "Score2 " << s.score[1] << "\t";
     cout << "Sum " << s.sum << "\t";
     cout << "Avg " << s.avg << "\n";
+    }
 }
