33982da6737206a15476869852dbc00fe15c1a55
SaulMFlores <saulmflores@Live.com>
2021-09-14 19:19:22 -0700

    cout is iffy

--- assignment8-3.cpp
+++ assignment8-3.cpp
@@ -1,15 +1,46 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+int findMin(int [], int, int);
+void printArray(int [], int);
 
 int main()
 {
-    const int N = 10;
-    int temp;
-    int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
-    for (int i = 0; i < N; i++){
-        temp = numbers[i];
-        if (temp < numbers[i]){
-            //swap it
-        }
-    }
+	const int N = 10;
+	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55,20,50};
+	int minidx;
+
+	printArray(numbers, N);
+
+	for(int i=0;i<N-1;i++)
+    {
+        minidx= findMin(numbers, N, i); // Find the i_th smallest number
+		swap(numbers[i], numbers[minidx]);
+	}
+
+	printArray(numbers, N);
+
+}
+
+int findMin(int numbers[], int N, int ith)
+{
+	int min, minidx, i;
+	min = numbers[ith];
+    for(i=ith ;i<N;i++)
+	{
+		if ( min > numbers[i] )
+		{
+			min = numbers[i];
+			minidx = i;
+		}
+	}
+	return i; // 1
+}
+
+void printArray(int a[], int x)
+{
+	for(int i = 0; i < x; i++){
+    cout << a[i] << "\t";
+  }
+  cout << "\n";
 }
