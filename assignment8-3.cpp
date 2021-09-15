4910e18113e3c97363ca2edd9d4cec24773c3615
SaulMFlores <saulmflores@Live.com>
2021-09-14 18:59:11 -0700

    second commit

---
+++ assignment8-3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    const int N = 10;
+    int temp;
+    int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
+    for (int i = 0; i < N; i++){
+        temp = numbers[i];
+        if (temp < numbers[i]){
+            //swap it
+        }
+    }
+}
