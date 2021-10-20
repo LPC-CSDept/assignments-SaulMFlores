f0afd3e4371110449ce81c990b712e6adf940873
SaulMFlores <saulmflores@Live.com>
2021-10-20 09:06:24 -0700

    changed line 18 from out to in

--- quiz2/q2/q2-2read.cpp
+++ quiz2/q2/q2-2read.cpp
@@ -6,6 +6,7 @@ struct Student{
     char name[20];
     int id;
     int scores[3];
+    struct Student* next;
 };
 
 int main()
@@ -15,20 +16,22 @@ cout << "does this work?";
     Student *sptr;
     sptr = new Student;
     Student s = *sptr;
-      ifs.open("students.bin", ios::out|ios::binary);
+      ifs.open("students.bin", ios::in|ios::binary);
     if(!ifs)
     {
       cout << "Failed to open. students.bin not found";
     }
     
     ifs.read((char*)&sptr, sizeof(sptr));
+    {
    for ( int i = 0; i<sizeof(sptr); i++)
-   {
+   {    
         ifs >> (sptr+i)->id >> (sptr+i)->name;
         for(int j = 0; j<3; j++)
         {
             ifs >> (sptr+i)->scores[j];
         }
+    }
     }
     cout << "thers supposed to be something posted here -> " << s.id << endl;
 }
