f28a49a00764ecfc8b6b630a8311aad50ccf0a31
SaulMFlores <saulmflores@Live.com>
2021-10-19 23:56:26 -0700

    Got it to write now

--- quiz2/q2/q2-2write.cpp
+++ quiz2/q2/q2-2write.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
 #include <fstream>
 
-
 using namespace std;
 
 struct Student{
     char name[20];
     int id;
-    int scores[3];
+    double scores[3];
 };
 
+
+Student *makeStudents(int n);
 
 main(){
  ifstream ifs;
  ofstream ofs;
- Student *sptr;
- sptr = new Student;
- 
- 
- ifs.open("students.txt");
- ofs.open("students.bin");
- for ( int i = 0; i<10; i++){
-        ifs >> (sptr+i)->id >> (sptr+i)->name;
-        for(int j = 0; j<3; j++){
-            ifs >> (sptr+i)->scores[j];
-        }
-        ofs.write((char*) &sptr, sizeof(sptr));
+     const int N = 10;
+    Student *ptr=nullptr;
+    ptr = makeStudents(N);
+    ofs.open("students.bin");
+    for (int i = 0; i<N; i++){
+        ofs.write((char*) &ptr, sizeof(ptr));
     }
- ofs.close();
-    delete sptr;
+}
+
+Student *makeStudents(int N){
+    Student   *ptr = new Student [10];
+    
+    ifstream ifs;
+
+    ifs.open("students.txt");
+    if ( ifs.fail()){
+        cerr << "File open error\n";
+        exit(0);
+    }
+    for ( int i = 0; i<N; i++){
+        ifs >> (ptr+i)->id >> (ptr+i)->name;
+        for(int j = 0; j<3; j++)
+            ifs >> (ptr+i)->scores[j];    
+    }
+
+    return ptr;
 }
