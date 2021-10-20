1121d476a7ac8f9b0c9b02b644e086d38d283f06
SaulMFlores <saulmflores@Live.com>
2021-10-20 07:27:06 -0700

    oh god what am I doing

--- quiz2/q2/q2-2write.cpp
+++ quiz2/q2/q2-2write.cpp
@@ -1,44 +1,87 @@
 #include <iostream>
 #include <fstream>
-
 using namespace std;
 
 struct Student{
     char name[20];
     int id;
     double scores[3];
+    struct Student* next;
 };
 
 
-Student *makeStudents(int n);
+Student makeStudents(int n);
 
 main(){
- ifstream ifs;
- ofstream ofs;
+
      const int N = 10;
-    Student *ptr=nullptr;
-    ptr = makeStudents(N);
-    ofs.open("students.bin");
-    for (int i = 0; i<N; i++){
-        ofs.write((char*) &ptr, sizeof(ptr));
+     makeStudents(N);
+    //  for (int i = 0; i<N; i++){
+      //  ofs.write((char*) &ptr, sizeof(ptr));
     }
 }
 
-Student *makeStudents(int N){
-    Student   *ptr = new Student [10];
+Student makeStudents(int N){
     
     ifstream ifs;
+    ofstream ofs;
 
     ifs.open("students.txt");
     if ( ifs.fail()){
         cerr << "File open error\n";
         exit(0);
     }
-    for ( int i = 0; i<N; i++){
-        ifs >> (ptr+i)->id >> (ptr+i)->name;
-        for(int j = 0; j<3; j++)
-            ifs >> (ptr+i)->scores[j];    
-    }
+    ofs.open("students.bin");
+  
+    Student *new_student, *head, *ptr, *prev;
 
+	head = nullptr;
+
+	for(int i=0; i<N; i++)
+	{
+		new_student = new Student;
+
+        ofs.write((int*) &id, sizeof(ptr));
+		cin >> new_student->id;
+		new_student->next = nullptr;
+
+		if ( head == nullptr)
+		{
+			head = new_student;
+		}
+		else
+		{
+			prev = nullptr;
+			ptr = head ;
+			while( ptr != nullptr)
+			{
+				if( new_student->id < ptr->id)
+					 break;
+				else
+				{
+					prev = ptr;
+					ptr = ptr->next;
+				}
+			}
+			if ( prev == nullptr)
+			{
+				head = new_student;
+				new_student->next = ptr;
+			}
+			else
+			{
+				new_student->next = prev->next;
+				prev->next = new_student;
+			}
+		}
+	}
+    
+    ptr = head;
+    int i = 1;
+	while(ptr != nullptr)
+    {
+		cout << i++ << " node's value " << ptr->id << endl;
+		ptr = ptr->next;
+    }
     return ptr;
 }
