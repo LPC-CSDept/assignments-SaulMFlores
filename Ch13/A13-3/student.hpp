e218510fe9d2e61958fbee5b0125b3c495dc791f
SaulMFlores <saulmflores@Live.com>
2021-10-26 18:41:21 -0700

    first initialize.  Finished classes for hpp

---
+++ Ch13/A13-3/student.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class DOB{ 
+    private:
+    int month;
+    int day;
+    DOB dob;
+    public:
+    DOB();
+    DOB(int month,int day);
+    int getMonth() const;
+    int getDay() const;
+    void printDate() const;
+    void setDOB(DOB dob);
+    void setMonth (int month);
+    void setDay(int day);
+}
+
+class student{
+    private:
+    int sid;
+    string sname;
+    DOB dob;
+    public:
+    student();
+    student(string name, int sid, DOB dob)
+    int getSID() const;
+    string getSname() const;
+    DOB getDOB() const;
+    void setSname(string name);
+    void setSID(int sid);
+    void setDOB(DOB dob);
+};
