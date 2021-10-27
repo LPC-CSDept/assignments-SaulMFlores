c6e2738dee5c7c7b8972a7f5a005be4d1e88fd69
SaulMFlores <saulmflores@Live.com>
2021-10-26 19:02:20 -0700

    edited

--- Ch13/A13-3/student.hpp
+++ Ch13/A13-3/student.hpp
@@ -4,26 +4,24 @@ class DOB{
     private:
     int month;
     int day;
-    DOB dob;
     public:
     DOB();
     DOB(int month,int day);
-    int getMonth() const;
-    int getDay() const;
-    void printDate() const;
-    void setDOB(DOB dob);
-    void setMonth (int month);
+    int getMonth() const{ return month; }
+    int getDay() const { return day; }
+    void printDate() const { cout << month << "\t" << day;}
+    void setMonth (int month){ month = month; };
     void setDay(int day);
-}
+};
 
-class student{
+class Student{
     private:
     int sid;
     string sname;
     DOB dob;
     public:
-    student();
-    student(string name, int sid, DOB dob)
+    Student();
+    Student(string name, int sid, DOB dob);
     int getSID() const;
     string getSname() const;
     DOB getDOB() const;
