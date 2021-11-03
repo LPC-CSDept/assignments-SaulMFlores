efbce3ca5fce2fe4784e3c31c48c772255b84db2
SaulMFlores <saulmflores@Live.com>
2021-11-02 19:27:55 -0700

    defined some functions to numheader.hpp

--- Quiz3/numheader.hpp
+++ Quiz3/numheader.hpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <vector>
 #include <iostream>
+
 template<class T>
 class Numbers
 {
@@ -14,18 +15,50 @@ public:
 	Numbers() {ID = -1;}
 	Numbers(T id, int num) 
 	{ 
-		pool.reserve(id);
+		numbers.reserve(id);
 		for(int i=0;i<num; i++)
-			pool.push_back(rand() % 100); 
+			numbers.push_back(rand() % 100); 
 	}
-	int getID() const;
-	int getSize const;
-	int getElm(int i) const;
-	int getMax() const;
-	int getMin() const;
-	int getSum() const;
-	void deleteElm(int d);
-	void addElm(int a);
+	int getID() const 
+	{
+		return ID;
+	}
+	int getSize() const
+	{
+		return numbers.size();
+	}
+	int getElm(int i) const
+	{
+		return numbers.at(i);
+	}
+	int getMax() const
+	{
+		int max;
+		//maybe implement binary search to find max here
+		return max 
+	};
+	int getMin() const
+	{
+		int min;
+		//maybe implement binary search to find min here
+		return min;
+	}
+	int getSum() const
+	{
+		int sum;
+		for (int i = 0; i<numbers.size(); i++){
+			sum += numbers.at(i);
+		}
+		return sum;
+	}
+	void deleteElm(int d)
+	{
+
+	}
+	void addElm(int a)
+	{
+		numbers.pop_back(a);
+	}
 	void printAll() const;
 
 };
