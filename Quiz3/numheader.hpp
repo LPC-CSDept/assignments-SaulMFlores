b235baa33357e4d72248feed89f609c0eab0203c
SaulMFlores <saulmflores@Live.com>
2021-11-02 19:14:35 -0700

    finished template to header file+added constructor

--- Quiz3/numheader.hpp
+++ Quiz3/numheader.hpp
@@ -1,7 +1,9 @@
 #ifndef NUMHEADER_H
 #define NUMHEADER_H
-
+#include <cstdlib>
 #include <vector>
+#include <iostream>
+template<class T>
 class Numbers
 {
 private:
@@ -9,7 +11,22 @@ private:
 	vector<int> numbers;
 
 public:
-	Numbers()
+	Numbers() {ID = -1;}
+	Numbers(T id, int num) 
+	{ 
+		pool.reserve(id);
+		for(int i=0;i<num; i++)
+			pool.push_back(rand() % 100); 
+	}
+	int getID() const;
+	int getSize const;
+	int getElm(int i) const;
+	int getMax() const;
+	int getMin() const;
+	int getSum() const;
+	void deleteElm(int d);
+	void addElm(int a);
+	void printAll() const;
 
 };
 
